96ec0a880b7272dc0ea681b6a784a6c50293ece1
Ortizcarmen <carmen_c19@umes.edu.gt>
2025-10-23 18:18:21 -0600

    Fix: Add npm build to deployment

--- build.sh
+++ build.sh
@@ -1,6 +1,16 @@
 #!/usr/bin/env bash
+
+set -e
+
 composer install --no-dev --optimize-autoloader
+
+# IMPORTANTE: Instalar dependencias y compilar assets
+npm ci --no-audit
+npm run build
+
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
 php artisan migrate --force
+
+
