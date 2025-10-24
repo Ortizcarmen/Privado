83815de4b9b7ca2ed8f392c8d0417eb94dbd1a4c
Ortizcarmen <carmen_c19@umes.edu.gt>
2025-10-23 20:18:54 -0600

    Nuevos cambios

--- resources/views/components/forum/layouts/home.blade.php
+++ resources/views/components/forum/layouts/home.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Foro de programación</title>
 
-    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@3.4.0/dist/tailwind.min.css" rel="stylesheet">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
  
 
 </head>
