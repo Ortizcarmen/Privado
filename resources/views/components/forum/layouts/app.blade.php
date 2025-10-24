d479c09532d447a9fedb7bb0efc0bb5c57ab70b2
Ortizcarmen <carmen_c19@umes.edu.gt>
2025-10-23 21:34:07 -0600

    Back to regresoN

--- resources/views/components/forum/layouts/app.blade.php
+++ resources/views/components/forum/layouts/app.blade.php
@@ -5,8 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Foro de programación</title>
 
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
-    
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@3.4.0/dist/tailwind.min.css" rel="stylesheet">
 </head>
 <body class="min-h-screen bg-gradient-to-b from-neutral-950 to-neutral-900 text-white">
     <div class="px-4 border-b border-neutral-800">
