e844ddd2baa0c6a61d9b21993452b03f9be270f9
Ortizcarmen <carmen_c19@umes.edu.gt>
2025-10-23 18:41:55 -0600

    Update build.sh - skip npm build

--- build.sh
+++ build.sh
@@ -4,13 +4,11 @@ set -e
 
 composer install --no-dev --optimize-autoloader
 
-# IMPORTANTE: Instalar dependencias y compilar assets
-npm ci --no-audit
-npm run build
+# NO compilamos porque ya subimos public/build
+# npm ci --no-audit
+# npm run build
 
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
 php artisan migrate --force
-
-
