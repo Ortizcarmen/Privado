bcf18cbcc11c1660f612958b2cee2589a58f5e50
Ortizcarmen <carmen_c19@umes.edu.gt>
2025-10-23 21:13:24 -0600

    Nuevos retornon

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import laravel from 'laravel-vite-plugin';
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-    base: process.env.APP_URL + '/build/',
+    
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
